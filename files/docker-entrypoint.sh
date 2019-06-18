6b6bda9d15c13c43197f5b3acaf43b32786d21c0
saibotk <saibotk@users.noreply.github.com>
2019-06-18 15:14:23 +0200

    remove cave view and add index.json

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -16,26 +16,34 @@ WORLD_FOLDER_END=/opt/blockmap/end
 OUTPUT_DIR=/opt/blockmap/output
 BLOCKMAP_FILE=/opt/blockmap/BlockMap.jar
 
-# mkdir -p $OUTPUT_DIR
-ls -l /opt/blockmap
 # A simple and plain overworld view
 echo "[INFO] Rendering overworld..."
 mkdir -p $OUTPUT_DIR/overworld
 java -jar $BLOCKMAP_FILE -v render -l -o=$OUTPUT_DIR/overworld $WORLD_FOLDER_OVERWORLD
+
 # The ocean grounds of the overworld
 echo "[INFO] Rendering overworld_ocean..."
 mkdir -p $OUTPUT_DIR/overworld_ocean
 java -jar $BLOCKMAP_FILE -v render -l -o=$OUTPUT_DIR/overworld_ocean -c=OCEAN_GROUND $WORLD_FOLDER_OVERWORLD
-# All caves up to height 30
-echo "[INFO] Rendering overworld_cave..."
-mkdir -p $OUTPUT_DIR/overworld_cave
-java -jar $BLOCKMAP_FILE -v render -l -o=$OUTPUT_DIR/overworld_cave -c=CAVES --max-height=30 $WORLD_FOLDER_OVERWORLD
+
 # The nether up to height 64
 echo "[INFO] Rendering overworld_nether..."
 mkdir -p $OUTPUT_DIR/overworld_nether
 java -jar $BLOCKMAP_FILE -v render -l -o=$OUTPUT_DIR/nether --max-height=64 $WORLD_FOLDER_NETHER
+
 # A plain view of the end
 echo "[INFO] Rendering overworld_end..."
 mkdir -p $OUTPUT_DIR/overworld_end
 java -jar $BLOCKMAP_FILE -v render -l -o=$OUTPUT_DIR/end $WORLD_FOLDER_END
+
+# Write an index file to the root directory. It is a simple name=path properties file, but in JSON.
+cat << EOF > $OUTPUT_DIR/index.json
+{
+	"Overworld": "./overworld/rendered.json.gz",
+	"Overworld (Ocean)": "./overworld_ocean/rendered.json.gz",
+	"Nether": "./nether/rendered.json.gz",
+	"End": "./end/rendered.json.gz"
+}
+EOF
+
 echo "[INFO] Rendering finished!"
