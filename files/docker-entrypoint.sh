85d5cbb808ad06775eece4ec65b1df492f957cca
saibotk <git@saibotk.de>
2020-07-10 01:22:29 +0200

    Update default config & allow parameters

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "[INFO] Start rendering..."
 
-java -jar /opt/blockmap/blockmap.jar render-many /config.json
+java -jar /opt/blockmap/blockmap.jar render-many /config.json $@
 
 echo "[INFO] Rendering finished!"
